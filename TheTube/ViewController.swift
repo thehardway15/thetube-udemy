54937c933ff10f9d89aaa7a107217188aed6184e
Damian Wiśniewski <d.wisniewski@uniso.pl>
2018-05-13 12:48:30 +0200

    Initial Commit

---
+++ TheTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheTube
+//
+//  Created by Damian Wiśniewski on 13.05.2018.
+//  Copyright © 2018 Damian Wiśniewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
